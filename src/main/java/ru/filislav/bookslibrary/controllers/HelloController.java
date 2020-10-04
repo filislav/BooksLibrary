f8e19446d49616963f539f247e0078f5de2ef44f
Вячеслав Филиппов <vaceslavfilippov@iMac-Vaceslav.local>
2020-10-04 11:52:25 +0400

    init

---
+++ src/main/java/ru/filislav/bookslibrary/controllers/HelloController.java
@@ -0,0 +1,14 @@
+package ru.filislav.bookslibrary.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String hello(){
+        return "hello";
+    }
+}
