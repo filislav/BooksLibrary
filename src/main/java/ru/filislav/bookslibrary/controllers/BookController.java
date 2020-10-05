3398ee37874d154a5ccc2cd3b0dd4dd35e7fa81b
Вячеслав Филиппов <slava@MBP-Vaceslav.lan>
2020-10-05 17:21:33 +0400

    late

--- src/main/java/ru/filislav/bookslibrary/controllers/BookController.java
+++ src/main/java/ru/filislav/bookslibrary/controllers/BookController.java
@@ -1,11 +1,23 @@
 package ru.filislav.bookslibrary.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import ru.filislav.bookslibrary.domain.Book;
+import ru.filislav.bookslibrary.repository.BooksRepository;
 
 @Controller
 @RequestMapping("/books")
 public class BookController {
+    @Autowired
+    private BooksRepository booksRepository;
 
-
+    @GetMapping
+    public String index(Model model){
+        Iterable<Book> books = booksRepository.findAll();
+        model.addAttribute("books",books);
+        return "index";
+    }
 }
---
+++ src/main/java/ru/filislav/bookslibrary/dao/BookDao.java
@@ -0,0 +1,10 @@
+package ru.filislav.bookslibrary.dao;
+
+import org.springframework.stereotype.Component;
+import ru.filislav.bookslibrary.domain.Book;
+@Component
+public class BookDao {
+    private Book book;
+
+
+}
